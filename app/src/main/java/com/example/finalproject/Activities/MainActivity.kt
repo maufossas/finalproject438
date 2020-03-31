0ba786c0187bbb9914e85ee1989c71c2ab68b0db
patriciacollins <patriciacollins@wustl.edu>
2020-03-31 16:11:33 -0400

    wip login fragments

--- app/src/main/java/com/example/finalproject/Activities/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/Activities/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.finalproject.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.finalproject.Adapters.SigninTabsAdapter
 import com.example.finalproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //set up tabView
+        val fragAdapter =
+            SigninTabsAdapter(supportFragmentManager)
+        viewPager.adapter = fragAdapter
+
+        tab_layout.setupWithViewPager(viewPager)
     }
 }
---
+++ app/src/main/java/com/example/finalproject/Adapters/SigninTabsAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.finalproject.Adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.finalproject.Fragments.LoginFragment
+import com.example.finalproject.Fragments.SignupFragment
+
+
+class SigninTabsAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                LoginFragment();
+            }
+            1 -> {
+                SignupFragment();
+            }
+            else -> null!!
+        }
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return when (position) {
+            0 -> "Login"
+            1 -> "Sign Up"
+            else -> {
+                return ""
+            }
+        }
+    }
+}
