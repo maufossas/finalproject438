da9e4365a1109d57c509c8d86f2abf9ad15e9d25
patriciacollins <patriciacollins@wustl.edu>
2020-04-02 17:05:16 -0400

    wip search page, adding dropdown menus

--- app/src/main/java/com/example/finalproject/Fragments/SearchFragment.kt
+++ app/src/main/java/com/example/finalproject/Fragments/SearchFragment.kt
@@ -7,8 +7,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 
 import com.example.finalproject.R
+import kotlinx.android.synthetic.main.fragment_search.*
 
 
 class SearchFragment : Fragment() {
@@ -19,6 +21,23 @@ class SearchFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this.context!!,
+            R.array.languages_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            languageSpinner.adapter = adapter
+        }
+        languageSpinner.prompt = "Country"
     }
 
 }
